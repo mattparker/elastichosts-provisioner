54f2f88b4dafa3e2b33330c2a28584866d8941d5
Matt Parker <matt@lamplightdb.co.uk>
2014-06-23 15:27:21 +0100

    Credentials for testing on localhost

---
+++ php/set-eh-credentials-test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: matt
+ * Date: 23/06/14
+ * Time: 12:07
+ */
+putenv('EHAUTH=matt:hello');
+putenv('EHURI=http://localhost:8000');
+//define('EHEXPORTS', 'export EHAUTH=matt:hello && export EHURI=http://localhost:8000');
