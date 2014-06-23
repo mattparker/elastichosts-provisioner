c0ff549c9de0ae139dff8ecec51ab7c6f34eaddb
Matt Parker <matt@lamplightdb.co.uk>
2014-06-23 11:06:07 +0100

    Give drives an identifier

--- php/EHDrive.php
+++ php/EHDrive.php
@@ -7,10 +7,78 @@
 
 class EHDrive implements Drive {
 
+
+
+
+    /**
+     * @var Object
+     */
     private $config;
 
+    /**
+     * @var string
+     */
+    private $identifier;
+
+
+
+
+
+    /**
+     * @param $config Object
+     */
     public function __construct ($config) {
         $this->config = $config;
     }
 
-} 
+
+
+
+    /**
+     * @return string
+     */
+    public function getName () {
+        if (!property_exists($this->config, 'name')) {
+            return '';
+        }
+        return $this->config->name;
+    }
+
+
+
+
+    /**
+     * @return int
+     */
+    public function getSize () {
+        if (!property_exists($this->config, 'size')) {
+            return 0;
+        }
+        return $this->config->size;
+    }
+
+
+
+
+    /**
+     * The end-point generated ID for this drive.
+     *
+     * @param $id String
+     */
+    public function setIdentifier ($id) {
+        $this->identifier = $id;
+    }
+
+
+
+
+    /**
+     * The end-point generated ID for this drive
+     *
+     * @return String
+     */
+    public function getIdentifier () {
+        return $this->identifier;
+    }
+
+}
