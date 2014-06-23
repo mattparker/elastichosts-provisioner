40117b4c3ac7368862e22b97228b708ab4cd75aa
Matt Parker <matt@lamplightdb.co.uk>
2014-06-23 15:24:28 +0100

    Add a logger

---
+++ php/EHLogger.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: matt
+ * Date: 23/06/14
+ * Time: 14:52
+ */
+
+trait EHLogger {
+
+
+    public function log ($msg) {
+
+        echo "\n -- LOG -- " . $msg . "\n";
+
+    }
+
+} 
