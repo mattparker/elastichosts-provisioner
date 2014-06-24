97250896d9fbd7af1bc4f39ae3125eef072d4bb9
Matt Parker <matt@lamplightdb.co.uk>
2014-06-24 10:47:31 +0100

    Some getters for ServerGroup

--- php/EHServerGroup.php
+++ php/EHServerGroup.php
@@ -7,15 +7,45 @@
 
 class EHServerGroup implements ServerGroup {
 
+    /**
+     * @var string
+     */
     private $name;
+
+
+    /**
+     * @var EHServer[]
+     */
     private $servers = [];
 
+
+    /**
+     * @param string $name
+     */
     public function __construct ($name) {
         $this->name = $name;
     }
 
+
+    /**
+     * @param Server $server
+     */
     public function addServer (Server $server) {
         $this->servers[] = $server;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName () {
+        return $this->name;
+    }
+
+    /**
+     * @return EHServer[]
+     */
+    public function getServers () {
+        return $this->servers;
     }
 
 
